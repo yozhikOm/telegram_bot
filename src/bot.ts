ca1207ebb1fcf72c4ece892471a8467a85ba02b0
Матрехина Татьяна Александровна <tatiana.matrehina@geropharm.com>
2025-08-14 17:00:53 +0300

    feat: add message filters and hears

--- src/bot.ts
+++ src/bot.ts
@@ -15,12 +15,36 @@ bot.command('hello', (ctx) => ctx.reply('Hello!'));
 
 // Menu
 bot.api.setMyCommands([
-{ command: 'start', description: 'Запуск бота' },
-{ command: 'hello', description: 'Получить приветствие' },
+  { command: 'start', description: 'Запуск бота' },
+  { command: 'hello', description: 'Получить приветствие' },
 ]);
 
-// Handle other messages.
-bot.on('message', (ctx) => ctx.reply('Got another message!'));
+//Hears
+bot.hears(['пинг', 'Пинг'], async (ctx) => {
+  await ctx.reply('понг');
+});
+
+bot.hears(/пипец/, async (ctx) => {
+  await ctx.reply('Ругаемся?');
+});
+
+bot.hears('ID', async (ctx) => {
+  await ctx.reply(`Ваш telegram ID: ${ctx.from!.id}`);
+});
+
+bot.on(
+  'message',
+  async (ctx) =>
+    await ctx.reply('Привет', {
+      reply_parameters: { message_id: ctx.msg.message_id },
+    })
+);
+
+//Different types of messages
+bot.on('message:photo', (ctx) => ctx.reply('Ваше сообщение содержит фото'));
+bot.on('message::url', (ctx) => ctx.reply('Ваше сообщение содержит url'));
+bot.on('::email', (ctx) => ctx.reply('Ваше сообщение содержит email'));
+bot.on('message:text', (ctx) => ctx.reply('Получил сообщение с текстом'));
 
 bot.catch((err) => {
   const ctx = err.ctx;
