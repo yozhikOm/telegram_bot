836bd6e82bea8360cdf596879386d1bb3d6f95fc
Матрехина Татьяна Александровна <tatiana.matrehina@geropharm.com>
2025-08-14 17:48:21 +0300

    feat: add keyboards

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, GrammyError, HttpError } from 'grammy';
+import { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } from 'grammy';
 import 'dotenv/config';
 
 const BOT_API_KEY = process.env.BOT_API_KEY;
@@ -17,6 +17,9 @@ bot.command('hello', (ctx) => ctx.reply('Hello!'));
 bot.api.setMyCommands([
   { command: 'start', description: 'Запуск бота' },
   { command: 'hello', description: 'Получить приветствие' },
+  { command: 'my_mood', description: 'Спроси у меня про настроение' },
+  { command: 'mood_assessment', description: 'Оцени мое настроение' },
+  { command: 'share', description: 'Поделиться данными' },
 ]);
 
 //Hears
@@ -32,20 +35,39 @@ bot.hears('ID', async (ctx) => {
   await ctx.reply(`Ваш telegram ID: ${ctx.from!.id}`);
 });
 
-bot.on(
-  'message',
-  async (ctx) =>
-    await ctx.reply('Привет', {
-      reply_parameters: { message_id: ctx.msg.message_id },
-    })
-);
+bot.hears('Хорошо', async (ctx) => {
+  await ctx.reply('Рад слышать!', {
+    reply_markup: { remove_keyboard: true },
+  });
+});
+
+bot.hears('Плохо', async (ctx) => {
+  await ctx.reply('Мне очень жаль!', {
+    reply_markup: { remove_keyboard: true },
+  });
+});
+
+// bot.on(
+//   'message',
+//   async (ctx) =>
+//     await ctx.reply('Привет', {
+//       reply_parameters: { message_id: ctx.msg.message_id },
+//     })
+// );
 
 //Different types of messages
 bot.on('message:photo', (ctx) => ctx.reply('Ваше сообщение содержит фото'));
 bot.on('message::url', (ctx) => ctx.reply('Ваше сообщение содержит url'));
 bot.on('::email', (ctx) => ctx.reply('Ваше сообщение содержит email'));
-bot.on('message:text', (ctx) => ctx.reply('Получил сообщение с текстом'));
+//bot.on('message:text', (ctx) => ctx.reply('Получил сообщение с текстом'));
+bot.on(':location', async (ctx) => {
+  await ctx.reply('Спасибо за геолокацию!');
+});
+bot.on(':contact', async (ctx) => {
+  await ctx.reply('Спасибо за контакт!');
+});
 
+// Errors
 bot.catch((err) => {
   const ctx = err.ctx;
   console.error(`Error while handling update ${ctx.update.update_id}:`);
@@ -57,6 +79,73 @@ bot.catch((err) => {
   } else {
     console.error('Unknown error:', error);
   }
+});
+
+// Keyboard
+const moodKeyboard = new Keyboard()
+  .text('Хорошо')
+  .row()
+  .text('Нормально')
+  .row()
+  .text('Плохо')
+  .resized()
+  .oneTime();
+
+const moodLabels = ['Хорошо', 'Нормально', 'Плохо'];
+const rows = moodLabels.map((label) => [Keyboard.text(label)]);
+const moodKeyboard2 = Keyboard.from(rows).resized().oneTime();
+
+bot.command('my_mood', async (ctx) => {
+  await ctx.reply('Как настроение?', {
+    reply_markup: moodKeyboard2,
+  });
+});
+
+const shareKeyboard = new Keyboard()
+  .requestLocation('Геолокация')
+  .requestContact('Контакт')
+  .requestPoll('Опрос')
+  .placeholder('Я хочу поделиться...')
+  .resized();
+
+bot.command('share', async (ctx) => {
+  await ctx.reply('Какими данными хочешь поделиться?', {
+    reply_markup: shareKeyboard,
+  });
+});
+
+const numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+//const rowsInline = numbers.map((num) => [InlineKeyboard.text(num.toString())]);
+//const inlineKeyboard = InlineKeyboard.from(rowsInline);
+const inlineKeyboard = new InlineKeyboard()
+  .text('1', 'button-1')
+  .text('2', 'button-2')
+  .text('3', 'button-3')
+  .text('4', 'button-4')
+  .text('5', 'button-5');
+
+bot.command('mood_assessment', async (ctx) => {
+  await ctx.reply('Оцените ваше настроение от 1 до 5', {
+    reply_markup: inlineKeyboard,
+  });
+});
+
+// bot.callbackQuery('button-1', async (ctx) => { 
+//     await ctx.reply('Выбрана цифра 1');
+// });
+// bot.callbackQuery('button-2', async (ctx) => { 
+//     await ctx.answerCallbackQuery({
+//         text: 'Выбрана цифра 2',
+//     });
+//     await ctx.answerCallbackQuery();
+// });
+// bot.callbackQuery('button-3', async (ctx) => { 
+//     await ctx.reply('Выбрана цифра 3');
+//     await ctx.answerCallbackQuery({ text: "Ответ принят!" });
+// });
+bot.on('callback_query:data', async (ctx) => {
+    await ctx.reply(`Нажата кнопка ${ctx.callbackQuery.data}`);
+    await ctx.answerCallbackQuery();
 });
 
 bot.start();
